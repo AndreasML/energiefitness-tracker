afd3a1ea4a54af915256b259e3238b10023ab75c
andreas armstrong <andreasarmstrong94@gmail.com>
2021-04-12 11:52:54 +0100

    get token dynamically

--- main.py
+++ main.py
@@ -19,14 +19,23 @@ def get_secret(secret):
     secret_string = response.payload.data.decode("UTF-8")
 
 
+def get_token(session):
+    headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+        }        
+    r = session.get(URL_LOGIN, headers=headers)#.cookies#[".AspNetCore.Identity.Application"]
+    soup = BeautifulSoup(r.text, 'html.parser')
+    token = soup.select('input[name="__RequestVerificationToken"]')[0]['value']
+    return token
+
+
 def login():
 
     # Persistent login session
     session = requests.session()
 
     # Get login auth token
-    result = session.get(URL_LOGIN)
-    tree = html.fromstring(result.text)
+    token = get_token(session)
 
     headers = {
         'authority': 'members.energiefitness.com',
@@ -35,20 +44,14 @@ def login():
         'origin': 'https://members.energiefitness.com',
         'content-type': 'application/x-www-form-urlencoded',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'referer': 'https://members.energiefitness.com/login/',
+        'referer': URL_LOGIN,
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        'cookie': 'OptanonAlertBoxClosed=2020-10-10T10:53:05.398Z; .AspNetCore.Antiforgery.RuGnUm8OIDw=CfDJ8J4QTDSFntBOk5KNSaeuQStASOpek_zO7iLwba91NO1MHvyr-uQQYjWUrM7RZEiqU1H1CN60W1i2zj4x2cbMy9v6vfznCcOE-tEs0Fc82ZPR9kXsVJ_zxTfyb1CNN61TUa3JlP0DIC7PexYq77Lg5-Y; .AspNetCore.Identity.Application=CfDJ8J4QTDSFntBOk5KNSaeuQSuUmjGnB2eTztyOXvIG_W4KSIaxoUQw5A5s_wZ3nGlHtmleM0ShR0fdB592mYcj3Jyx10XX83BbgSSl5jaa7HvE-aWQv4PHQU2DZ5okg-wwE9O9lxIcxqGDt0s4074x5FUnxm9iYuLj7lhweh-k76jaWBwMcmb0suY46FdSOXI97_8AXjzBFY-MuJJpXWfpklKt8wk5RkSJZepjaSh1SlNJT0IdpNQzrC4faBo6gEAkWHzRh_PlLpu2TKuIoE_Vb9eQgsD_h6vRC0NTJvSi_8bHJ3QHt05rksBzVRxDXAtUT07g9UCD0-BmvOci9NHHXvA8R_aMqq8Zso-gNEcr5BWpE0Hb5jXqFaTq-xQT4yG6h5mpYv6Yb0-hgUbKf-9ABJrmCwF4aNeO4Qa07LtFTl0p2tSW-Q1LAsD6SKbqlqDUxRROdHBQscIkEaP6q10kaIAyqp_WoZ9JFVl9efh9Pp9x7N8eVelchUcLcCbPkRNHWCA_4h1xUFXfDi3isFyVX7C0T1KHWGAIKXnE4Ufye_HBH5Jfr8sP3y96p1wACUpX4RvAZ1NrOZanRUUM2r-TUH49ZpRlwX503FP5ItcPtsyf; OptanonConsent=landingPath=NotLandingPage&datestamp=Wed+Oct+28+2020+12%3A00%3A03+GMT%2B0000+(Greenwich+Mean+Time)&version=3.6.28&groups=1%3A1%2C2%3A1%2C4%3A1%2C0_51523%3A1%2C0_51522%3A1&AwaitingReconsent=false',
     }
 
     payload = {
         'Email': get_secret(SECRET_EMAIL),
         'Password': get_secret(SECRET_PASSWORD),
-        '__RequestVerificationToken': get_secret(SECRET_REQUEST_TOKEN)
+        '__RequestVerificationToken': token
     }
 
     # Perform login
